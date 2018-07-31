fba50412b923fc21b2ba59d96019d8ef7eb04e02
Shiroko <hhx.xxm@gmail.com>
2018-07-31 23:05:53 +0800

    TOpBar created

---
+++ src/components/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="topbar">
+    <mu-appbar style="width: 100%;" title="Yabe"></mu-appbar>
+</div>
+</template>
+
+<style lang="less">
+#topbar {
+  background-color: red;
+}
+</style>
+
+<script>
+export default {};
+</script>
